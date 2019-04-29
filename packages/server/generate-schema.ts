8ee58fdb90506cfa46234faa5788576a1647711e
lostfictions <lostfictions@users.noreply.github.com>
2019-04-29 02:36:12 -0400

    testing gql-codegen

---
+++ packages/server/generate-schema.ts
@@ -0,0 +1,37 @@
+import { exec } from "child_process";
+
+import { ApolloServer } from "apollo-server";
+
+import typeDefs from "./src/typedefs";
+
+const mockServer = new ApolloServer({
+  typeDefs,
+  mocks: true
+});
+
+mockServer.listen().then(async ({ url, server }) => {
+  console.log(`Listening at ${url}`);
+
+  await new Promise(res => {
+    const child = exec(`graphql-codegen --config codegen.yml`, {
+      env: {
+        ...process.env,
+        URL: url
+      }
+    });
+
+    child.stdout.on("data", data => {
+      process.stdout.write(data);
+    });
+
+    child.stderr.on("data", data => {
+      process.stdout.write(data);
+    });
+
+    child.on("exit", res);
+  });
+
+  console.log("Done.");
+
+  server.close();
+});
