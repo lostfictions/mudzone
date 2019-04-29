d6da85b555076a3a7f932a8e50bc3033202db539
lostfictions <lostfictions@users.noreply.github.com>
2019-04-29 08:08:26 -0400

    schema updates

--- packages/client/src/generated/models.tsx
+++ packages/client/src/generated/models.tsx
@@ -16,51 +16,176 @@ export enum CacheControlScope {
   Private = "PRIVATE"
 }
 
+export enum Direction {
+  Up = "UP",
+  Down = "DOWN",
+  Left = "LEFT",
+  Right = "RIGHT"
+}
+
+export type Entity = {
+  id: Scalars["String"];
+  appearance: Scalars["String"];
+  color: Scalars["String"];
+  position: Position;
+};
+
+export type Message = {
+  channel: Scalars["String"];
+  author: Scalars["String"];
+  text: Scalars["String"];
+};
+
 export type Mutation = {
-  sendMessage?: Maybe<Scalars["String"]>;
+  move?: Maybe<Position>;
+  sendMessage?: Maybe<Message>;
+};
+
+export type MutationMoveArgs = {
+  direction: Direction;
 };
 
 export type MutationSendMessageArgs = {
-  author?: Maybe<Scalars["String"]>;
-  message?: Maybe<Scalars["String"]>;
+  channel: Scalars["String"];
+  message: Scalars["String"];
+};
+
+export type Position = {
+  x: Scalars["Int"];
+  y: Scalars["Int"];
 };
 
 export type Query = {
-  /** A simple type for getting started! */
-  hello?: Maybe<Scalars["String"]>;
+  room?: Maybe<Room>;
+};
+
+export type QueryRoomArgs = {
+  id: Scalars["String"];
+};
+
+export type Room = {
+  width: Scalars["Int"];
+  height: Scalars["Int"];
 };
 
 export type Subscription = {
-  booped?: Maybe<Scalars["String"]>;
+  entityChanged?: Maybe<Entity>;
+  messageReceived?: Maybe<Message>;
 };
 
-export type BoopedSubscriptionVariables = {};
+export type SubscriptionEntityChangedArgs = {
+  id: Scalars["String"];
+};
 
-export type BoopedSubscription = { __typename?: "Subscription" } & Pick<
-  Subscription,
-  "booped"
->;
+export type SubscriptionMessageReceivedArgs = {
+  channel?: Maybe<Scalars["String"]>;
+};
+
+export type EntityChangedSubscriptionVariables = {
+  id: Scalars["String"];
+};
+
+export type EntityChangedSubscription = { __typename?: "Subscription" } & {
+  entityChanged: Maybe<
+    { __typename?: "Entity" } & {
+      position: { __typename?: "Position" } & Pick<Position, "x" | "y">;
+    }
+  >;
+};
+
+export type MessageReceivedSubscriptionVariables = {
+  channel: Scalars["String"];
+};
+
+export type MessageReceivedSubscription = { __typename?: "Subscription" } & {
+  messageReceived: Maybe<
+    { __typename?: "Message" } & Pick<Message, "author" | "text">
+  >;
+};
+
+export type MoveMutationVariables = {
+  direction: Direction;
+};
+
+export type MoveMutation = { __typename?: "Mutation" } & {
+  move: Maybe<{ __typename?: "Position" } & Pick<Position, "x" | "y">>;
+};
+
+export type SendMessageMutationVariables = {
+  channel: Scalars["String"];
+  message: Scalars["String"];
+};
+
+export type SendMessageMutation = { __typename?: "Mutation" } & {
+  sendMessage: Maybe<{ __typename?: "Message" } & Pick<Message, "text">>;
+};
 import { DocumentNode } from "graphql";
+import * as React from "react";
+import * as ReactApollo from "react-apollo";
 import * as ReactApolloHooks from "react-apollo-hooks";
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-export const BoopedDocument: DocumentNode = {
+export const EntityChangedDocument: DocumentNode = {
   kind: "Document",
   definitions: [
     {
       kind: "OperationDefinition",
       operation: "subscription",
-      name: { kind: "Name", value: "Booped" },
-      variableDefinitions: [],
+      name: { kind: "Name", value: "EntityChanged" },
+      variableDefinitions: [
+        {
+          kind: "VariableDefinition",
+          variable: { kind: "Variable", name: { kind: "Name", value: "id" } },
+          type: {
+            kind: "NonNullType",
+            type: { kind: "NamedType", name: { kind: "Name", value: "String" } }
+          },
+          directives: []
+        }
+      ],
       directives: [],
       selectionSet: {
         kind: "SelectionSet",
         selections: [
           {
             kind: "Field",
-            name: { kind: "Name", value: "booped" },
-            arguments: [],
-            directives: []
+            name: { kind: "Name", value: "entityChanged" },
+            arguments: [
+              {
+                kind: "Argument",
+                name: { kind: "Name", value: "id" },
+                value: { kind: "Variable", name: { kind: "Name", value: "id" } }
+              }
+            ],
+            directives: [],
+            selectionSet: {
+              kind: "SelectionSet",
+              selections: [
+                {
+                  kind: "Field",
+                  name: { kind: "Name", value: "position" },
+                  arguments: [],
+                  directives: [],
+                  selectionSet: {
+                    kind: "SelectionSet",
+                    selections: [
+                      {
+                        kind: "Field",
+                        name: { kind: "Name", value: "x" },
+                        arguments: [],
+                        directives: []
+                      },
+                      {
+                        kind: "Field",
+                        name: { kind: "Name", value: "y" },
+                        arguments: [],
+                        directives: []
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
           }
         ]
       }
@@ -68,14 +193,336 @@ export const BoopedDocument: DocumentNode = {
   ]
 };
 
-export function useBoopedSubscription(
+export const EntityChangedComponent = (
+  props: Omit<
+    Omit<
+      ReactApollo.SubscriptionProps<
+        EntityChangedSubscription,
+        EntityChangedSubscriptionVariables
+      >,
+      "subscription"
+    >,
+    "variables"
+  > & { variables?: EntityChangedSubscriptionVariables }
+) => (
+  <ReactApollo.Subscription<
+    EntityChangedSubscription,
+    EntityChangedSubscriptionVariables
+  >
+    subscription={EntityChangedDocument}
+    {...props}
+  />
+);
+
+export function useEntityChangedSubscription(
   baseOptions?: ReactApolloHooks.SubscriptionHookOptions<
-    BoopedSubscription,
-    BoopedSubscriptionVariables
+    EntityChangedSubscription,
+    EntityChangedSubscriptionVariables
   >
 ) {
   return ReactApolloHooks.useSubscription<
-    BoopedSubscription,
-    BoopedSubscriptionVariables
-  >(BoopedDocument, baseOptions);
+    EntityChangedSubscription,
+    EntityChangedSubscriptionVariables
+  >(EntityChangedDocument, baseOptions);
+}
+export const MessageReceivedDocument: DocumentNode = {
+  kind: "Document",
+  definitions: [
+    {
+      kind: "OperationDefinition",
+      operation: "subscription",
+      name: { kind: "Name", value: "MessageReceived" },
+      variableDefinitions: [
+        {
+          kind: "VariableDefinition",
+          variable: {
+            kind: "Variable",
+            name: { kind: "Name", value: "channel" }
+          },
+          type: {
+            kind: "NonNullType",
+            type: { kind: "NamedType", name: { kind: "Name", value: "String" } }
+          },
+          directives: []
+        }
+      ],
+      directives: [],
+      selectionSet: {
+        kind: "SelectionSet",
+        selections: [
+          {
+            kind: "Field",
+            name: { kind: "Name", value: "messageReceived" },
+            arguments: [
+              {
+                kind: "Argument",
+                name: { kind: "Name", value: "channel" },
+                value: {
+                  kind: "Variable",
+                  name: { kind: "Name", value: "channel" }
+                }
+              }
+            ],
+            directives: [],
+            selectionSet: {
+              kind: "SelectionSet",
+              selections: [
+                {
+                  kind: "Field",
+                  name: { kind: "Name", value: "author" },
+                  arguments: [],
+                  directives: []
+                },
+                {
+                  kind: "Field",
+                  name: { kind: "Name", value: "text" },
+                  arguments: [],
+                  directives: []
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ]
+};
+
+export const MessageReceivedComponent = (
+  props: Omit<
+    Omit<
+      ReactApollo.SubscriptionProps<
+        MessageReceivedSubscription,
+        MessageReceivedSubscriptionVariables
+      >,
+      "subscription"
+    >,
+    "variables"
+  > & { variables?: MessageReceivedSubscriptionVariables }
+) => (
+  <ReactApollo.Subscription<
+    MessageReceivedSubscription,
+    MessageReceivedSubscriptionVariables
+  >
+    subscription={MessageReceivedDocument}
+    {...props}
+  />
+);
+
+export function useMessageReceivedSubscription(
+  baseOptions?: ReactApolloHooks.SubscriptionHookOptions<
+    MessageReceivedSubscription,
+    MessageReceivedSubscriptionVariables
+  >
+) {
+  return ReactApolloHooks.useSubscription<
+    MessageReceivedSubscription,
+    MessageReceivedSubscriptionVariables
+  >(MessageReceivedDocument, baseOptions);
+}
+export const MoveDocument: DocumentNode = {
+  kind: "Document",
+  definitions: [
+    {
+      kind: "OperationDefinition",
+      operation: "mutation",
+      name: { kind: "Name", value: "Move" },
+      variableDefinitions: [
+        {
+          kind: "VariableDefinition",
+          variable: {
+            kind: "Variable",
+            name: { kind: "Name", value: "direction" }
+          },
+          type: {
+            kind: "NonNullType",
+            type: {
+              kind: "NamedType",
+              name: { kind: "Name", value: "Direction" }
+            }
+          },
+          directives: []
+        }
+      ],
+      directives: [],
+      selectionSet: {
+        kind: "SelectionSet",
+        selections: [
+          {
+            kind: "Field",
+            name: { kind: "Name", value: "move" },
+            arguments: [
+              {
+                kind: "Argument",
+                name: { kind: "Name", value: "direction" },
+                value: {
+                  kind: "Variable",
+                  name: { kind: "Name", value: "direction" }
+                }
+              }
+            ],
+            directives: [],
+            selectionSet: {
+              kind: "SelectionSet",
+              selections: [
+                {
+                  kind: "Field",
+                  name: { kind: "Name", value: "x" },
+                  arguments: [],
+                  directives: []
+                },
+                {
+                  kind: "Field",
+                  name: { kind: "Name", value: "y" },
+                  arguments: [],
+                  directives: []
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ]
+};
+export type MoveMutationFn = ReactApollo.MutationFn<
+  MoveMutation,
+  MoveMutationVariables
+>;
+
+export const MoveComponent = (
+  props: Omit<
+    Omit<
+      ReactApollo.MutationProps<MoveMutation, MoveMutationVariables>,
+      "mutation"
+    >,
+    "variables"
+  > & { variables?: MoveMutationVariables }
+) => (
+  <ReactApollo.Mutation<MoveMutation, MoveMutationVariables>
+    mutation={MoveDocument}
+    {...props}
+  />
+);
+
+export function useMoveMutation(
+  baseOptions?: ReactApolloHooks.MutationHookOptions<
+    MoveMutation,
+    MoveMutationVariables
+  >
+) {
+  return ReactApolloHooks.useMutation<MoveMutation, MoveMutationVariables>(
+    MoveDocument,
+    baseOptions
+  );
+}
+export const SendMessageDocument: DocumentNode = {
+  kind: "Document",
+  definitions: [
+    {
+      kind: "OperationDefinition",
+      operation: "mutation",
+      name: { kind: "Name", value: "SendMessage" },
+      variableDefinitions: [
+        {
+          kind: "VariableDefinition",
+          variable: {
+            kind: "Variable",
+            name: { kind: "Name", value: "channel" }
+          },
+          type: {
+            kind: "NonNullType",
+            type: { kind: "NamedType", name: { kind: "Name", value: "String" } }
+          },
+          directives: []
+        },
+        {
+          kind: "VariableDefinition",
+          variable: {
+            kind: "Variable",
+            name: { kind: "Name", value: "message" }
+          },
+          type: {
+            kind: "NonNullType",
+            type: { kind: "NamedType", name: { kind: "Name", value: "String" } }
+          },
+          directives: []
+        }
+      ],
+      directives: [],
+      selectionSet: {
+        kind: "SelectionSet",
+        selections: [
+          {
+            kind: "Field",
+            name: { kind: "Name", value: "sendMessage" },
+            arguments: [
+              {
+                kind: "Argument",
+                name: { kind: "Name", value: "channel" },
+                value: {
+                  kind: "Variable",
+                  name: { kind: "Name", value: "channel" }
+                }
+              },
+              {
+                kind: "Argument",
+                name: { kind: "Name", value: "message" },
+                value: {
+                  kind: "Variable",
+                  name: { kind: "Name", value: "message" }
+                }
+              }
+            ],
+            directives: [],
+            selectionSet: {
+              kind: "SelectionSet",
+              selections: [
+                {
+                  kind: "Field",
+                  name: { kind: "Name", value: "text" },
+                  arguments: [],
+                  directives: []
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ]
+};
+export type SendMessageMutationFn = ReactApollo.MutationFn<
+  SendMessageMutation,
+  SendMessageMutationVariables
+>;
+
+export const SendMessageComponent = (
+  props: Omit<
+    Omit<
+      ReactApollo.MutationProps<
+        SendMessageMutation,
+        SendMessageMutationVariables
+      >,
+      "mutation"
+    >,
+    "variables"
+  > & { variables?: SendMessageMutationVariables }
+) => (
+  <ReactApollo.Mutation<SendMessageMutation, SendMessageMutationVariables>
+    mutation={SendMessageDocument}
+    {...props}
+  />
+);
+
+export function useSendMessageMutation(
+  baseOptions?: ReactApolloHooks.MutationHookOptions<
+    SendMessageMutation,
+    SendMessageMutationVariables
+  >
+) {
+  return ReactApolloHooks.useMutation<
+    SendMessageMutation,
+    SendMessageMutationVariables
+  >(SendMessageDocument, baseOptions);
 }
--- packages/server/src/typedefs.ts
+++ packages/server/src/typedefs.ts
@@ -2,15 +2,46 @@ import { gql } from "apollo-server";
 
 export default gql`
   type Subscription {
-    booped: String
+    entityChanged(id: String!): Entity!
+    messageReceived(channel: String): Message!
   }
 
   type Mutation {
-    sendMessage(author: String, message: String): String
+    move(direction: Direction!): Position
+    sendMessage(channel: String!, message: String!): Message
   }
 
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    room(id: String!): Room
+  }
+
+  type Position {
+    x: Int!
+    y: Int!
+  }
+
+  type Message {
+    channel: String!
+    author: String!
+    text: String!
+  }
+
+  type Entity {
+    id: String!
+    appearance: String!
+    color: String!
+    position: Position!
+  }
+
+  type Room {
+    width: Int!
+    height: Int!
+  }
+
+  enum Direction {
+    UP
+    DOWN
+    LEFT
+    RIGHT
   }
 `;
