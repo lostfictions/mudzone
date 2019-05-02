b66b2c5603c9444275994bc684333b672ae0735c
lostfictions <lostfictions@users.noreply.github.com>
2019-05-02 17:23:10 -0400

    tidying up eslint config

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -1,47 +1,30 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: `${__dirname}/tsconfig.json`,
-    sourceType: "module",
-    ecmaFeatures: {
-      modules: true
-    }
-  },
-  settings: { react: { version: "detect" } },
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
   extends: [
-    "eslint:recommended",
+    // eslint's `extends` can be a bit confusing -- it's tempting to declare all
+    // necessary rule extensions again here, but extending `eslint:recommended`
+    // again (since it's already extended in the root file) will *re-override*
+    // our overrides defined in the root file. on the other hand, `prettier`
+    // (and `prettier/@typescript-eslint`) need to go at the *end* of the
+    // extends chain, so we *do* re-declare them a second time here. (we keep
+    // them in the base file so nothing will break if we want to use the latter
+    // as a standalone config.)
     "plugin:react/recommended",
     "prettier",
     "prettier/@typescript-eslint"
   ],
+  env: {
+    browser: true
+  },
+  parserOptions: {
+    // This seemed like the least contrived way to point `@typescript-eslint` to
+    // the correct nested config file -- it was searching for one in the cwd
+    // instead.
+    project: `${__dirname}/tsconfig.json`
+  },
+  settings: { react: { version: "detect" } },
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   rules: {
-    curly: ["warn", "multi-line", "consistent"],
-    "no-console": "off",
-    "no-dupe-class-members": "error",
-    "no-empty": "warn",
-    "no-undef": "error",
-    "no-unused-expressions": "error",
-    "no-unused-vars": "off",
-    "no-var": "warn",
-    "prefer-const": "warn",
-    "no-shadow": "warn",
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        args: "none",
-        ignoreRestSiblings: true
-      }
-    ]
+    "react-hooks/exhaustive-deps": "warn"
   }
 };
--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -1,38 +1,6 @@
+// See comments and explanations in packages/client/.eslintrc.js.
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true
-  },
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: `${__dirname}/tsconfig.json`,
-    sourceType: "module",
-    ecmaFeatures: {
-      modules: true
-    }
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "prettier", "prettier/@typescript-eslint"],
-  rules: {
-    curly: ["warn", "multi-line", "consistent"],
-    "no-console": "off",
-    "no-dupe-class-members": "error",
-    "no-empty": "warn",
-    "no-undef": "error",
-    "no-unused-expressions": "error",
-    "no-unused-vars": "off",
-    "no-var": "warn",
-    "prefer-const": "warn",
-    "no-shadow": "warn",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        args: "none",
-        ignoreRestSiblings: true
-      }
-    ]
+    project: `${__dirname}/tsconfig.json`
   }
 };
