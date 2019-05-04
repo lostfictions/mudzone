6e7015184ab5540a9030a17171a8a9c6ab8aaf21
lostfictions <lostfictions@users.noreply.github.com>
2019-05-04 18:18:51 -0400

    tooling/comments

--- packages/server/src/store/db-types.ts
+++ packages/server/src/store/db-types.ts
@@ -1,6 +1,10 @@
 import { Entity } from "../generated/graphql";
 import { FilterProperties } from "../util/filter-properties";
 
+// these types represent models retrieved from the database, rather than those
+// exposed to clients. the models can thus contain both private data and
+// unresolved references to other objects.
+
 export interface EntityDbObject
   extends FilterProperties<Entity, { room: any }> {
   room: string;
